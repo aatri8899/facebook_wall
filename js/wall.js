248f8e4d0f0865d050ba82fe666ce085f7b73937
aatri8899 <aatri8899@withs.org>
2016-03-22 14:13:12 +0000

    Facebook wall project

--- js/wall.js
+++ js/wall.js
@@ -2,5 +2,13 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
+
 	// YOUR CODE GOES HERE
+	$("#post-btn").click(function() {
+		var text = $("#message").val();
+	     $("#wall").append("<div>" + "<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text  + "</div>");
+
+		  $("img").width(150);
+		   $("img").css("margin-right", 20);
+	});
 });
